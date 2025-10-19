62ffabdc284d5cba0ff2a93503ae20eb8062f1b3
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-19 13:29:32 +0200

    fix : -update the method listContainsO       -rename the variables

--- src/main/java/sprint1Tasca_S108Nivell1_1/ListContains_oMain.java
+++ src/main/java/sprint1Tasca_S108Nivell1_1/ListContains_oMain.java
@@ -3,6 +3,7 @@ package sprint1Tasca_S108Nivell1_1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -15,15 +16,15 @@ import java.util.List;
  */
 public class ListContains_oMain {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<String>(Arrays.asList("Own", "bowl", "fine", "Offside", "object", "good", "nice"));
-        List <String> listNew = listContainsO(list);
-        listNew.forEach(System.out::println);
+        List<String> listInput = new ArrayList<String>(Arrays.asList("Own", "bowl", "fine", "Offside", "object", "good", "nice"));
+        List <String> listResult = listContainsO(listInput);
+        listResult.forEach(System.out::println);
 
     }
 
     public static  List<String> listContainsO(List<String> list) {
 
-        return list.stream().filter(s -> s.toLowerCase().contains("o")).toList();
+        return list.stream().filter(s -> s!=null && s.toLowerCase().contains("o")).collect(Collectors.toList());
     }
 
 }
