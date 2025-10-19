6ba5359141c3d29917f19e6317fae527cfdd66b7
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-19 16:07:53 +0200

    feat : -add interface MyFunctional

---
+++ src/main/java/sprint1Tasca_S108Nivell1_5/MyFunctional.java
@@ -0,0 +1,13 @@
+package sprint1Tasca_S108Nivell1_5;
+
+/**
+ * ClassName: MyFunctional
+ * Package: sprint1Tasca_S108Nivell1_5
+ * Description:
+ * Author: Rong Jiang
+ * Create:19/10/2025 - 16:06
+ * Version:v1.0
+ */
+public interface MyFunctional {
+    public double getPiValue();
+}
