73e82b24f7fb059ae99d5bb53eaa5992e82dbd19
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-19 16:21:28 +0200

    feat : -add @FunctionalInterface fix :  -update the main code

--- src/main/java/sprint1Tasca_S108Nivell1_5/MyFunctional.java
+++ src/main/java/sprint1Tasca_S108Nivell1_5/MyFunctional.java
@@ -3,11 +3,13 @@ package sprint1Tasca_S108Nivell1_5;
 /**
  * ClassName: MyFunctional
  * Package: sprint1Tasca_S108Nivell1_5
- * Description:
+ * Description:Functional interface to provide the value of Pi (3.1415)
  * Author: Rong Jiang
  * Create:19/10/2025 - 16:06
  * Version:v1.0
  */
+
+@FunctionalInterface
 public interface MyFunctional {
     public double getPiValue();
 }
--- src/main/java/sprint1Tasca_S108Nivell1_5/MyFunctionalMain.java
+++ src/main/java/sprint1Tasca_S108Nivell1_5/MyFunctionalMain.java
@@ -3,7 +3,7 @@ package sprint1Tasca_S108Nivell1_5;
 /**
  * ClassName: MyFunctionalMain
  * Package: sprint1Tasca_S108Nivell1_5
- * Description:
+ * Description: Main class to demonstrate the use of MyFunctional interface to print Pi value
  * Author: Rong Jiang
  * Create:19/10/2025 - 16:08
  * Version:v1.0
@@ -11,6 +11,6 @@ package sprint1Tasca_S108Nivell1_5;
 public class MyFunctionalMain {
     public static void main(String[] args) {
         MyFunctional myFunctional = ()-> 3.1415;
-        System.out.println(myFunctional);
+        System.out.println(myFunctional.getPiValue());
     }
 }
