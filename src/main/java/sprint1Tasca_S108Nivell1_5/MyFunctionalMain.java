c276b39fe966a1aeff788964d33862f6a3d83e19
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-19 16:15:15 +0200

    feat : -add MyFunctionalMain

---
+++ src/main/java/sprint1Tasca_S108Nivell1_5/MyFunctionalMain.java
@@ -0,0 +1,16 @@
+package sprint1Tasca_S108Nivell1_5;
+
+/**
+ * ClassName: MyFunctionalMain
+ * Package: sprint1Tasca_S108Nivell1_5
+ * Description:
+ * Author: Rong Jiang
+ * Create:19/10/2025 - 16:08
+ * Version:v1.0
+ */
+public class MyFunctionalMain {
+    public static void main(String[] args) {
+        MyFunctional myFunctional = ()-> 3.1415;
+        System.out.println(myFunctional);
+    }
+}
